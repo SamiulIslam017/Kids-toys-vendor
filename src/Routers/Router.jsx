05f2df8aa0716063307a5888e2cffd58db585436
SamiulIslam017 <samiul641@gmail.com>
2023-05-18 17:14:25 +0600

    Header work Done

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,6 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
+import AllToys from "../Pages/AllToys";
+import MyToys from "../Pages/MyToys";
+import AddNewToy from "../Pages/AddNewToy";
+import Blogs from "../Pages/Blogs";
+import SingleToyDetails from "../Pages/SingleToyDetails";
+import Login from "../Pages/Login/Login"
+import Register from "../Pages/Login/Register"
+
 import Error from "../Pages/Error";
 
 
@@ -14,6 +22,35 @@ const router = createBrowserRouter([
         path:'/',
         element:<Home></Home>
     },
+    {
+      path:'/allToys',
+      element: <AllToys></AllToys>
+    },
+    {
+      path:'/myToys',
+      element:<MyToys></MyToys>
+    
+    },
+    {
+      path:'/addAToy',
+      element:<AddNewToy></AddNewToy>
+    },
+    {
+      path:'/blogs',
+      element:<Blogs></Blogs>
+    },
+    {
+      path:"/toyDetails",
+      element:<SingleToyDetails></SingleToyDetails>
+    },
+    {
+      path:"/login",
+      element:<Login></Login>
+    },
+    {
+      path:'/register',
+      element:<Register></Register>
+    }
     
 ],
 },
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,11 +1,134 @@
-
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <div>
-            navbar
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="w-full bg-white shadow-2xl py-4">
+      <div className="navbar max-w-7xl mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                  Home
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <img
+            src="https://i.ibb.co/WxTw08d/39cc8ddffab5435abb08bfa5b997d6df-removebg-preview-1.png"
+            className="w-28"
+          />
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/allToys"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                All Toys
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/myToys"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                My Toys
+              </NavLink>
+            </li>
+
+            {user && (
+              <li>
+                <NavLink
+                  to="/AddAToy"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                  Add A Toy
+                </NavLink>
+              </li>
+            )}
+
+            <li>
+              <NavLink
+                to="/blogs"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Blog
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          {user ? (
+            <div className="flex gap-3 items-center">
+              <span>
+                {user.photoURL === null ? (
+                  <FaUserCircle className="text-2xl" title={user.displayName} />
+                ) : (
+                  <img
+                    className="w-10 h-10 rounded-full object-cover"
+                    src={user.photoURL}
+                    title={user.displayName}
+                  />
+                )}
+              </span>
+              <button
+                onClick={handleLogOut}
+                className="text-white bg-red-500 text-md  font-bold rounded-lg py-3 px-6 tracking-widest cursor-pointer"
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <Link to="/login">
+              <button className="btn primary-bg-color border-0">Login</button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
